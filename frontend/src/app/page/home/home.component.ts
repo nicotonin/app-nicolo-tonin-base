b442fe708a26607d8ce8e9223f0b58938ed4c178
nicotonin <nicotonin05@gmail.com>
2026-06-08 12:28:33 +0200

    commit right

---
+++ frontend/src/app/page/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { BehaviorSubject, catchError, map, of, switchMap } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent{
+
+}
---
+++ frontend/src/app/service/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from './user.entity';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  protected http = inject(HttpClient);
+
+  list(role: string) {
+  return this.http.get<User[]>(
+    `${environment.apiUrl}/users`,
+    { params: { role } }
+  );
+}
+
+
+}
